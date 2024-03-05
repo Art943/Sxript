82412ba0b6ddf6c6651b47665e8fc8f89728a263
Jonatan <jonatanfriman@hotmail.com>
2024-03-05 15:02:25 +0100

    generate.py now opens json file and creates directories test/signals

---
+++ script/generate.py
@@ -0,0 +1,39 @@
+import json
+from pathlib import Path
+import shutil
+import os
+
+ROOT = Path(__file__).parent
+
+try:
+    # Opening JSON file using 'with open'
+    with open('data.json', 'r') as f:
+        # returns JSON object as a dictionary
+        data = json.load(f)
+
+    # Iterating through the json list
+    for i in data['signals']:
+        print(i)
+
+except FileNotFoundError:
+    print("File not found. Please check the file path.")
+except json.JSONDecodeError:
+    print("Error decoding JSON. Please check if the file contains valid JSON.")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
+
+
+TEST_DIR = Path(ROOT.parent, 'test')
+SIGNALS_DIR = Path(ROOT.parent, 'lib', 'signals')
+
+print(TEST_DIR)
+
+try:
+    shutil.rmtree(TEST_DIR, True)
+    shutil.rmtree(SIGNALS_DIR, True)
+    os.makedirs(TEST_DIR, exist_ok=True)
+    os.makedirs(SIGNALS_DIR, exist_ok=True)
+
+except:
+    print("Error creating directories")
+    exit(1)
