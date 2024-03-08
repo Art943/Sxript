69d03417f8412f0f7810e0f1a039284a6ffd19e8
pernilla <stahlbring@gmail.com>
2024-03-08 13:59:21 +0100

    commit to text.py

--- script/text.py
+++ script/text.py
@@ -1,14 +1,18 @@
 def generate_signals_txt(data):
+    
+    signal_length = max(len(signal['name']) for signal in data['signals'])
+    type_lengt = max(len(signal['type']) for signal in data['signals'])
+    range_values_lenght = max(len(', '.join(data['defines'][signal['values']])) if 'values' in signal else len(str(signal['range'])) for signal in data['signals'])
+    description_length = max(len(signal['comment']) for signal in data['signals'])
+
     with open('../lib/signals/signals.txt', 'w') as file:
-        file.write(f"{'Signal':<35} | {'Type':<10} | {'Range/Values':<20} | {'Description'}\n")
-        file.write('-' * 100 + '\n')
+        file.write(f"{'Signal':<{signal_length}} | {'Type':<{type_lengt}} | {'Range/Values':<{range_values_lenght}} | {'Description':<{description_length}}\n")
+        file.write('-' * (signal_length + type_lengt + range_values_lenght + description_length + 9) + '\n')
+        
         for signal in data['signals']:
             name = signal['name']
             type_ = signal['type']
-            range_values = signal.get('range', signal.get('values'))
-            if isinstance(range_values, str):  
-                range_values = ', '.join(data['defines'][range_values])
-            else:
-                range_values = str(range_values)
+            range_values = ', '.join(data['defines'][signal['values']]) if 'values' in signal else str(signal['range'])
             description = signal['comment']
-            file.write(f"{name:<35} | {type_:<10} | {range_values:<20} | {description}\n")
+            file.write(f"{name:<{signal_length}} | {type_:<{type_lengt}} | {range_values:<{range_values_lenght}} | {description:<{description_length}}\n")
+
