c663f0ced02fb2cbbac23ffe74c3556e17c4d2ff
Artush <artush.simonjan@yaelev.se>
2024-10-31 09:50:07 +0100

    first commit

--- script/text.py
+++ script/text.py
@@ -3,7 +3,7 @@ def generate_signals_txt(data):
     type_lengt = max(len(signal['type']) for signal in data['signals'])
     range_values_lenght = max(len(', '.join(data['defines'][signal['values']])) if 'values' in signal else len(str(signal['range'])) for signal in data['signals'])
     description_length = max(len(signal['comment']) for signal in data['signals'])
-
+    
     with open('../lib/signals/signals.txt', 'w') as file:
         file.write(f"{'Signal':<{signal_length}} | {'Type':<{type_lengt}} | {'Range/Values':<{range_values_lenght}} | {'Description':<{description_length}}\n")
         file.write('-' * (signal_length + type_lengt + range_values_lenght + description_length + 9) + '\n')
@@ -13,5 +13,5 @@ def generate_signals_txt(data):
             type_ = signal['type']
             range_values = ', '.join(data['defines'][signal['values']]) if 'values' in signal else str(signal['range'])
             description = signal['comment']
-            file.write(f"{name:<{signal_length}} | {type_:<{type_lengt}} | {range_values:<{range_values_lenght}} | {description:<{description_length}}\n")
+            file.write(f"{name:<{signal_length}} | {type_:<{type_lengt}} | {range_values:<{range_values_lenght}} | {description:<{description_length}} | \n")
 
